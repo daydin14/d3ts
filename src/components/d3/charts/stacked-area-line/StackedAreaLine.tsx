53815e13d386fc992c64bb32b8ecc4c635d8a809
David Aydin <david.aydin6@gmail.com>
2023-06-03 00:37:34 -0400

    WIP: SAL- imports, props, main sizing

--- src/components/d3/charts/stacked-area-line/StackedAreaLine.tsx
+++ src/components/d3/charts/stacked-area-line/StackedAreaLine.tsx
@@ -1,6 +1,34 @@
-interface StackedAreaLineProps {}
-const StackedAreaLine: React.FC<StackedAreaLineProps> = ({}) => {
-  return <></>;
+// Styling
+import "../../styles.css";
+
+// Dependencies
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+interface StackedAreaLineProps {
+  width: number;
+  height: number;
+}
+
+const StackedAreaLine: React.FC<StackedAreaLineProps> = ({ width, height }) => {
+  const chartRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (!chartRef.current) return;
+
+    // Reference the Chart SVG Element
+    const svg = d3
+      .select(chartRef.current)
+      .attr("width", width)
+      .attr("height", height);
+  }, [width, height]);
+  return (
+    <>
+      <div id="stacked-area-line-chart">
+        <svg ref={chartRef} />
+      </div>
+    </>
+  );
 };
 
 export default StackedAreaLine;
