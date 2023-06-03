7eece9dc05392a8e4a41d6943fec918cb47ca26e
David Aydin <david.aydin6@gmail.com>
2023-06-03 10:40:18 -0400

    Brushing, Paths, Line, Legend, Hover on Legend

--- src/components/d3/charts/stacked-area-line/StackedAreaLine.tsx
+++ src/components/d3/charts/stacked-area-line/StackedAreaLine.tsx
@@ -25,6 +25,7 @@ const StackedAreaLine: React.FC<StackedAreaLineProps> = ({
   height,
 }) => {
   const chartRef = useRef<SVGSVGElement>(null);
+  const legendRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     if (!chartRef.current) return;
@@ -142,11 +143,169 @@ const StackedAreaLine: React.FC<StackedAreaLineProps> = ({
       .text("Counts")
       .attr("text-anchor", "start")
       .style("fill", "steelblue");
+
+    //////////
+    // BRUSHING AND CHART //
+    //////////
+
+    // Add a clipPath: everything out of this area won't be drawn
+    svg
+      .append("defs")
+      .append("svg:clipPath")
+      .attr("id", "clip")
+      .append("svg:rect")
+      .attr("width", chartWidth)
+      .attr("height", chartHeight)
+      .attr("x", 0)
+      .attr("y", 0);
+
+    // Add brushing
+    const brush = d3
+      .brushX()
+      .extent([
+        [0, 0],
+        [chartWidth, chartHeight],
+      ])
+      .on("end", updateChart);
+
+    const areaChart = svg.append("g").attr("clip-path", "url(#clip)");
+
+    // Create the area generator
+    const area = d3
+      .area()
+      .x((d: any) => xScale(d.data.year))
+      .y0((d: any) => yScale(d[0]))
+      .y1((d: any) => yScale(d[1]));
+
+    // Show the areas
+    areaChart
+      .selectAll(".layers")
+      .data(stackedData)
+      .join("path")
+      .attr("class", (d: any) => "myArea " + d.key)
+      .style("fill", (d: any) => colorScale(d.key) as string)
+      .attr("d", area as any);
+
+    // Add the brushing
+    areaChart.append("g").attr("class", "brush").call(brush);
+
+    // Update chart feature
+    let idleTimeout: number | null;
+
+    function idled() {
+      idleTimeout = null;
+    }
+
+    // A function that updates the chart for given boundaries
+    function updateChart(event: d3.D3BrushEvent<any>, _d: any) {
+      let extent = event.selection;
+
+      // If no selection, back to the initial coordinate. Otherwise, update X axis domain
+      if (!extent) {
+        if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350)); // This allows to wait a little bit
+        xScale.domain(d3.extent(data, (d) => d.year) as [number, number]);
+      } else {
+        xScale.domain([
+          xScale.invert(extent[0] as number),
+          xScale.invert(extent[1] as number),
+        ]);
+        areaChart.select(".brush").call(brush.move as any, null); // This removes the grey brush area as soon as the selection has been done
+      }
+
+      // Update axis and area position
+      xAxis.transition().duration(1000).call(d3.axisBottom(xScale).ticks(5));
+      areaChart
+        .selectAll("path")
+        .transition()
+        .duration(1000)
+        .attr("d", area as any);
+    }
+    //////////
+    // HIGHLIGHT GROUP //
+    //////////
+
+    // What to do when one group is hovered
+    const highlight = (_event: any, d: any) => {
+      d3.selectAll(".myArea").style("opacity", 0.1); // reduce opacity of all groups
+      d3.select("." + d).style("opacity", 1); // except the one that is hovered
+    };
+
+    // And when it is not hovered anymore
+    const noHighlight = () => {
+      d3.selectAll(".myArea").style("opacity", 1);
+    };
+
+    // Line plot generator
+    const line = d3
+      .line()
+      .x((d: any) => xScaleLine(d.year))
+      .y((d: any) => yScaleLine(d.avgCount));
+
+    //////////
+    // LINE PLOT //
+    //////////
+
+    // Add the line plot
+    lineGroup
+      .append("path")
+      .datum(avgCounts)
+      .attr("class", "line")
+      .attr("d", line as any);
+
+    //////////
+    // LEGEND  //
+    //////////
+
+    // Add one dot in the legend for each name
+    const size = 20;
+    const barLegend = d3.select(legendRef.current);
+
+    barLegend
+      .append("g")
+      .attr("class", "stacked-area-line-legend")
+      .selectAll(".legend-rect")
+      .data(keys)
+      .join("rect")
+      .attr("class", "legend-rect")
+      .attr("x", 0)
+      .attr("y", (_d, i) => i * (size + 5) + size / 2)
+      .attr("width", size)
+      .attr("height", size)
+      .style("fill", (d) => colorScale(d) as string)
+      .on("mouseover", highlight)
+      .on("mouseleave", noHighlight);
+
+    // Add legend text
+    barLegend
+      .select(".stacked-area-line-legend")
+      .selectAll(".legend-label")
+      .data(keys)
+      .join("text")
+      .attr("class", "legend-label")
+      .attr("x", 25)
+      .attr("y", (_d, i) => 5 + i * (size + 5) + size / 2)
+      .style("fill", (d) => colorScale(d) as string)
+      .text((d) => d.toUpperCase())
+      .attr("text-anchor", "left")
+      .style("alignment-baseline", "middle")
+      .on("mouseover", highlight)
+      .on("mouseleave", noHighlight);
+
+    // CSS styling for the line
+    svg.append("style").text(`
+        .line {
+            fill: none;
+            stroke: black;
+            stroke-width: 10px;
+        }
+    `);
   }, [data, width, height]);
+
   return (
     <>
       <div id="stacked-area-line-chart">
         <svg ref={chartRef} />
+        <svg ref={legendRef} width={250} height={height / 2} />
       </div>
     </>
   );
