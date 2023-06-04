fe3a03208761d548f63a15708ddda6cda5ffb35c
David Aydin <david.aydin6@gmail.com>
2023-06-04 08:28:49 -0400

    Adjusted page display + removed old css

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,60 +1,10 @@
-// Styling
-import "./rstyles.css";
-
 // Components
-import HorizontalBarChart from "../components/d3/charts/bar/HorizontalBarChart";
-import ButterflyChart from "../components/d3/charts/butterfly/ButterflyChart";
-import SingleStack from "../components/d3/charts/single-stack/SingleStack";
-import NavMenu from "../components/nav/NavMenu";
-
-// Mock Data
-const bardata = [
-  { label: "HTML", value: 10 },
-  { label: "CSS", value: 20 },
-  { label: "JavaScript", value: 30 },
-  { label: "TypeScript", value: 40 },
-  { label: "React", value: 50 },
-];
-
-const singlestackdata = [
-  { category: "HTML", value: 10 },
-  { category: "CSS", value: 20 },
-  { category: "JavaScript", value: 30 },
-  { category: "TypeScript", value: 40 },
-  { category: "React", value: 50 },
-];
-
-const butterflydata = [
-  { age: 5, gender: "M", value: 2 },
-  { age: 15, gender: "M", value: 12 },
-  { age: 25, gender: "M", value: 22 },
-  { age: 35, gender: "M", value: 32 },
-  { age: 45, gender: "M", value: 42 },
-
-  { age: 5, gender: "F", value: 2 },
-  { age: 15, gender: "F", value: 12 },
-  { age: 25, gender: "F", value: 22 },
-  { age: 35, gender: "F", value: 32 },
-  { age: 45, gender: "F", value: 42 },
-];
+import Carousel from "../components/ carousel/Carousel";
 
 const Root = () => {
   return (
     <>
-      <NavMenu />
-      <div id="charts">
-        <HorizontalBarChart
-          data={bardata}
-          width={800}
-          height={800}
-          xtitle="Skill Level"
-          ytitle="Coding"
-        />
-        <br />
-        <SingleStack data={singlestackdata} width={800} height={800} />
-        <br />
-        <ButterflyChart data={butterflydata} width={800} height={800} />
-      </div>
+      <Carousel />
     </>
   );
 };
