f3f9c999847bdf299ef6b9a772c4163c538075ad
daydin14 <david.aydin6@gmail.com>
2023-06-19 11:45:22 -0400

    Vite, TS, & npm dependency config fixes & updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    cors: true,
+  },
+  base: "/",
+  build: {
+    outDir: "build",
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      input: "src/main.tsx",
+      plugins: [react()],
+    },
+    modulePreload: {
+      polyfill: true,
+    },
+    // watch: {
+    //   include: 'src/**',
+    //   exclude: 'node_modules/**',
+    // },
+  },
+  preview: {
+    host: "localhost",
+    port: 8000,
+    strictPort: true,
+    https: false,
+    cors: true,
+  },
+});
